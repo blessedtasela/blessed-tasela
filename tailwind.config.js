4c7075dc89ed87f9b617a1736c050ccf13fd57a5
Blessed Tasela <taselablessed@gmail.com>
2023-09-29 16:38:43 +0100

    first commit adding page

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'dark-blue': '#0A192F',
+      },
+    },
+  },
+  plugins: [],
+}
+
